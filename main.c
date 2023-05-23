cb11e050d07cecf325a85752ad8e62071c12567a
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-23 03:01:08 -0700

    Added monty.h, main.c, and stack.c with initial implementation

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    instruction_t opcodes[] = {
+        {"push", push},
+        {"pall", pall},
+        {NULL, NULL}
+    };
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        line_number++;
+        /* Parse instruction and argument from line */
+        /* Find corresponding function in opcodes array and call it */
+    }
+
+    fclose(file);
+
+    if (line)
+        free(line);
+
+    exit(EXIT_SUCCESS);
+}
+
