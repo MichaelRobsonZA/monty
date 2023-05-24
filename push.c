5b75fcf2a6f046154a727c77fa34ea11a7b159ab
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-24 01:33:41 -0700

    added implementation for push and pall opcodes

---
+++ push.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack * @stack: double pointer to the beginning of the stack
+ * @line_number: current line number in the file *
+ * Return: void */
+void push(stack_t **stack, unsigned int line_number)
+{
+    /* TODO: implement push opcode */
+}
+
