68e30a30d2bf7392ecd418e4e9ff63e47a597657
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-24 17:33:49 -0700

    Add error handling functions

---
+++ error.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * get_instruction - retrieves instruction function associated with opcode
+ * @opcode: the opcode to search for
+ *
+ * Return: pointer to instruction function, or NULL if not found
+ **/
+instruction_t *get_instruction(char *opcode)
+{
+    static instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        /* add more opcodes here */
+        {NULL, NULL}
+    };
+
+    int i;
+
+    for (i = 0; instructions[i].opcode != NULL; i++)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+            return (&instructions[i]);
+    }
+
+    return (NULL);
+}
+
+/**
+ * free_stack - frees a stack
+ * @stack: pointer to the top of the stack
+ */
+void free_stack(stack_t *stack)
+{
+    stack_t *temp;
+
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+}
+
